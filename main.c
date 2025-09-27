08a0f2c146fdc60ceb7e73afcbaf73068a254c31
hicham-darw <hamoudhicham234@gmail.com>
2025-09-27 19:43:19 +0100

    main for test project

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
 #include "get_next_line.h"
 #include <stdio.h>
-
+#include <fcntl.h>
 
 int	main(int ac, char **av)
 {
-	int		fd;
-	char	*line;
+	int			fd;
+	char		*line;
+	static int	i = 1;
 
 	if (ac != 2)
 		return (1);
-
 	fd = open(av[1], O_RDONLY);
 	if (fd == -1)
 	{
@@ -18,8 +18,8 @@ int	main(int ac, char **av)
 	}
 	while (get_next_line(fd, &line) > 0)
 	{
-		static int i = 1;
-		printf("%d - %s\n", i++, line);
-		free(line);
+		printf("%d - %s\n", i, line);
+		i++;
+		free (line);
 	}
 }
