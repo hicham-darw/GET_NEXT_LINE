da60b0bd761eebce6782dfc7b10fa8ada74260d3
hicham-darw <hamoudhicham234@gmail.com>
2025-09-27 19:16:10 +0100

    gnl as finished

--- get_next_line.c
+++ get_next_line.c
@@ -1,5 +1,66 @@
 #include "get_next_line.h"
-#include <stdio.h>
+
+static int	line_stashed(char **line, char **stashed)
+{
+	char	*pos;
+	char	*tmp;
+
+	if (!stashed || !*stashed)
+		return (-1);
+	pos = ft_strchr(*stashed, '\n');
+	if (pos)
+	{
+		*line = ft_strndup(*stashed, (pos - *stashed));
+		tmp = ft_strdup(*stashed + (pos - *stashed) + 1);
+		free(*stashed);
+		*stashed = ft_strdup(tmp);
+		free(tmp);
+	}
+	else if (!pos)
+	{
+		*line = ft_strdup(*stashed);
+		free(*stashed);
+		*stashed = ft_strdup("");
+	}
+	return (1);
+}
+
+static int	remaining_stashed(char **line, char **stashed)
+{
+	if (!stashed || !*stashed)
+		return (-1);
+	if (ft_strlen(*stashed) >= 1)
+		return (line_stashed(line, stashed));
+	free(*stashed);
+	return (0);
+}
+
+static int	get_line_append_buffer(char **line, char **stashed, char *buffer)
+{
+	char	*pos;
+	char	*tmp;
+
+	if (!stashed || !*stashed || !buffer)
+		return (-1);
+	pos = ft_strchr(*stashed, '\n');
+	if (pos)
+	{
+		*line = ft_strndup(*stashed, (pos - *stashed));
+		tmp = ft_strdup(*stashed + (pos - *stashed) + 1);
+	}
+	else
+		tmp = ft_strdup(*stashed);
+	free(*stashed);
+	*stashed = ft_strnew(sizeof(char) * (ft_strlen(tmp) + ft_strlen(buffer)));
+	if (!*stashed)
+		return (-1);
+	ft_strcpy(*stashed, tmp);
+	ft_strcat(*stashed, buffer);
+	free (tmp);
+	if (pos)
+		return (1);
+	return (0);
+}
 
 static int	get_line_from_buffer(char **line, char **stashed, char *buffer)
 {
@@ -9,41 +70,14 @@ static int	get_line_from_buffer(char **line, char **stashed, char *buffer)
 
 	if (!stashed || !*stashed || !buffer)
 		return (-1);
-
-	// check stashed first!
-	pos = ft_strchr(*stashed, '\n');
-	if (pos)
-	{
-		(void)len_s;
-		*line = ft_strndup(*stashed, (pos - *stashed));
-		tmp = ft_strdup(*stashed + (pos - *stashed) + 1);
-		free(*stashed);
-		*stashed = ft_strnew(sizeof(char) * (ft_strlen(tmp) + ft_strlen(buffer)));
-		if (!*stashed)
-			return (-1);
-		ft_strcpy(*stashed, tmp);
-		ft_strcat(*stashed, buffer);
-		free (tmp);
-		return (1);
-	}
-	else
-	{
-		tmp = ft_strdup(*stashed);
-		free(*stashed);
-		*stashed = ft_strnew(sizeof(char) * (ft_strlen(tmp) + ft_strlen(buffer)));
-		if (!stashed)
-			return (-1);
-		ft_strcpy(*stashed, tmp);
-		ft_strcat(*stashed, buffer);
-		free(tmp);
-	}
-	
-	// get line buffer now!
+	len_s = get_line_append_buffer(line, stashed, buffer);
+	if (len_s == 1)
+		return (len_s);
 	len_s = ft_strlen(*stashed);
 	pos = ft_strchr(*stashed, '\n');
 	if (pos)
 	{
-		*line = (char *)ft_strnew(sizeof(char) * (pos - *stashed));
+		*line = ft_strnew(sizeof(char) * (pos - *stashed));
 		if (!*line)
 			return (-1);
 		ft_strncpy(*line, *stashed, (pos - *stashed));
@@ -53,7 +87,6 @@ static int	get_line_from_buffer(char **line, char **stashed, char *buffer)
 		free(tmp);
 		return (1);
 	}
-	// no lines let stashed before in &stashed stashed everything!
 	return (0);
 }
 
@@ -61,7 +94,6 @@ int	get_next_line(const int fd, char **line)
 {
 	int			bytes_read;
 	char		buffer[BUFF_SIZE + 1];
-	char		*pos, *tmp;
 	static char	*stashed = NULL;
 
 	if (fd < 0 || !line)
@@ -69,8 +101,6 @@ int	get_next_line(const int fd, char **line)
 	if (!stashed)
 		stashed = ft_strdup("");
 	ft_memset(buffer, 0, BUFF_SIZE + 1);
-
-	// reading
 	bytes_read = read(fd, buffer, BUFF_SIZE);
 	while (bytes_read > 0)
 	{
@@ -85,32 +115,6 @@ int	get_next_line(const int fd, char **line)
 	}
 	if (bytes_read == -1)
 		return (-1);
-	else if (bytes_read == 0)
-	{
-		if (ft_strlen(stashed) >= 1)
-		{
-			pos = ft_strchr(stashed, '\n');
-			if (pos)
-			{
-				*line = ft_strndup(stashed, (pos - stashed));
-				tmp = ft_strdup(stashed + (pos - stashed) + 1);
-				free(stashed);
-				stashed = ft_strdup(tmp);
-				free(tmp);
-			}
-			else
-			{
-				*line = ft_strdup(stashed);
-				free(stashed);   /// look foor
-				stashed = ft_strdup("");
-			}
-			return (1);
-		}
-		else
-		{
-			free(stashed);
-			return (0);
-		}
-	}
-	return (ft_strlen(*line));
+	else
+		return (remaining_stashed(line, &stashed));
 }
