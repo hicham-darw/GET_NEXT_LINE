ac2e7275732bfc1a2ae5b31c279d0e857b42413e
hicham-darw <hamoudhicham234@gmail.com>
2025-09-27 08:02:51 +0100

    header for gnl

--- get_next_line.h
+++ get_next_line.h
@@ -1,12 +1,9 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <unistd.h>
-# include <fcntl.h>
-# include <stdlib.h>
 # include "libft.h"
 
-# define BUFF_SIZE 1
+# define BUFF_SIZE 10
 
 int		get_next_line(const int fd, char **line);
 
