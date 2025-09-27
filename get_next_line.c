5171f08d8640ac841d926eb98078e7c7add1943c
hicham-darw <hamoudhicham234@gmail.com>
2025-09-27 08:02:29 +0100

    fix errors

--- get_next_line.c
+++ get_next_line.c
@@ -1,64 +1,52 @@
 #include "get_next_line.h"
 
-static int	ft_fill_stashed(char **stashed, char *buffer)
+
+static int	get_line_from_buffer(char **line, char **stashed, char *buffer)
 {
 	int		len_stashed;
-	char	*tmp;
+	char	*pos;
 
-	if (!stashed)
-		return (-1);
 	len_stashed = 0;
 	if (*stashed)
 		len_stashed = ft_strlen(*stashed);
-	tmp = (char *)malloc(sizeof(char) * (len_stashed + ft_strlen(buffer) + 1));
-	if (!tmp)
+	pos = ft_strchr(buffer, '\n');
+	if (pos)
+		*line = (char *)ft_strnew(sizeof(char) * (len_stashed + (pos - buffer)));
+	else
+		*line = (char *)ft_strnew(sizeof(char) * (len_stashed + ft_strlen(buffer)));
+	if (!*line)
 		return (-1);
 	if (*stashed)
 	{
-		tmp = ft_strncpy(tmp, *stashed, ft_strlen(*stashed));
-		tmp = ft_strncat(tmp, buffer, ft_strlen(buffer));
+		ft_strncpy(*line, *stashed, ft_strlen(*stashed));
+		free(*stashed);
+		*stashed = NULL;
 	}
-	else
-		tmp = ft_strncpy(tmp, buffer, ft_strlen(buffer));
-	free(*stashed);
-	*stashed = tmp;
-	ft_memset(buffer, 0, BUFF_SIZE);
-	return (1);
-}
-
-static int	ft_get_line_from_buffer(char **l, char **stash, char *buf, char *p)
-{
-	int		len_stashed;
-
-	len_stashed = 0;
-	if (*stash)
-		len_stashed = ft_strlen(*stash);
-	*l = (char *)malloc(sizeof(char) * (len_stashed + (p - buf) + 1));
-	if (!*l)
-		return (-1);
-	if (*stash)
+	if (pos)
 	{
-		*l = ft_strncpy(*l, *stash, ft_strlen(*stash));
-		*l = ft_strncat(*l, buf, (p - buf));
+		ft_strncat(*line, buffer, (pos - buffer));
+		*stashed = ft_strdup(buffer + (pos - buffer) + 1);
+		return (1);
 	}
 	else
-		*l = ft_strncpy(*l, buf, (p - buf));
-	free(*stash);
-	*stash = ft_strdup(buf + (p - buf) + 1);
-	return (1);
+	{
+		ft_strncat(*line, buffer, ft_strlen(buffer));
+		*stashed = ft_strdup(*line);
+		free(*line);
+		return (0);
+	}
 }
 
-static int	ft_get_line_from_stashed(char **line, char **stashed, char *pos)
+static int	get_line_from_stashed(char **line, char *stashed, char *pos)
 {
 	char	*tmp;
 
-	*line = (char *)malloc(sizeof(char) * (pos - *stashed) + 1);
-	if (!*line)
+	if (!line || !*line || !pos || !stashed)
 		return (-1);
-	*line = ft_strncpy(*line, *stashed, (pos - *stashed));
-	tmp = ft_strdup(*stashed + (pos - *stashed) + 1);
-	free(*stashed);
-	*stashed = ft_strdup(tmp);
+	*line = ft_strndup(stashed, (pos - stashed));
+	tmp = ft_strdup(stashed + (pos - stashed) + 1);
+	free(stashed);
+	stashed = ft_strdup(tmp);
 	free (tmp);
 	return (1);
 }
@@ -76,18 +64,25 @@ int	get_next_line(const int fd, char **line)
 		stashed = ft_strdup("");
 	pos = ft_strchr(stashed, '\n');
 	if (pos)
-		return (ft_get_line_from_stashed(line, &stashed, pos));
-	bytes_read = read(fd, buffer, BUFF_SIZE);
-	if (bytes_read > 0)
+		return (get_line_from_stashed(line, stashed, pos));
+	ft_memset(buffer, 0, BUFF_SIZE + 1);
+	while ((bytes_read = read(fd, buffer, BUFF_SIZE)) > 0)
 	{
 		buffer[bytes_read] = '\0';
-		pos = ft_strchr(buffer, '\n');
-		if (pos)
-			return (ft_get_line_from_buffer(line, &stashed, buffer, pos));
-		else
-			if (ft_fill_stashed(&stashed, buffer) == -1)
-				return (-1);
-		get_next_line(fd, line);
+		if (get_line_from_buffer(line, &stashed, buffer) == 1)
+		{
+			ft_memset(buffer, 0, BUFF_SIZE + 1);
+			return (1);
+		}
+		ft_memset(buffer, 0, BUFF_SIZE + 1);
 	}
-	return (bytes_read);
+	if (stashed)
+	{
+		*line = ft_strdup(stashed);
+		free(stashed);
+		stashed = NULL;
+	}
+	else
+		return (0);
+	return (ft_strlen(*line));
 }
